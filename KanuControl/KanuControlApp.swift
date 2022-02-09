806f9599608b1050dbf92c07b316e7b549520d7c
Chris Schog <c.schog@icloud.com>
2022-02-09 18:20:48 +0100

    Initial Commit

---
+++ KanuControl/KanuControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanuControlApp.swift
+//  KanuControl
+//
+//  Created by Christoph Schog on 09.02.22.
+//
+
+import SwiftUI
+
+@main
+struct KanuControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
